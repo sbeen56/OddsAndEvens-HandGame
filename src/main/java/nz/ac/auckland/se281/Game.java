a7f51a756a8714a99cddeede9c4c6bef42a5f5e5
Subeen Ban <sban919@aucklanduni.ac.nz>
2024-05-15 01:19:00 +1200

    Added message for number of fingers of AI.

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -7,11 +7,13 @@ import nz.ac.auckland.se281.Main.Difficulty;
 public class Game {
   private int round;
   private String player;
+  private AIPlayer aiPlayer;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
     player = options[0];
     round = 1;
+    aiPlayer = new AIPlayer(difficulty);
   }
 
   public void play() {
@@ -35,6 +37,7 @@ public class Game {
     }
 
     MessageCli.PRINT_INFO_HAND.printMessage(player, input);
+    aiPlayer.getMove();
     round++;
   }
 
