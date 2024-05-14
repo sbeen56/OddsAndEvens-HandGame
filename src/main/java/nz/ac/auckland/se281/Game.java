e0d5a25a99cecb4a16a4332c466e4819e9758b8a
Subeen Ban <sban919@aucklanduni.ac.nz>
2024-05-14 15:31:42 +1200

    Added number of round message to play.

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -5,12 +5,16 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 /** This class represents the Game is the main entry point. */
 public class Game {
+  private int round = 0;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
+    round++;
   }
 
-  public void play() {}
+  public void play() {
+    MessageCli.START_ROUND.printMessage(Integer.toString(round));
+  }
 
   public void endGame() {}
 
